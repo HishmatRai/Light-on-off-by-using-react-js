98d2b1bca5a7b2f887c6bd110b59c50ba93f839a
Hishmat Rai <hishmatrai.2018@gmail.com>
2019-07-17 23:40:05 +0500

    Light On Of By Using React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import lightOff from './images/of.jpg'
+import lightOn from './images/on.jpg'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      light : false,
+      
+    }
+  }
+
+  render() {
+    const { light } = this.state;
+    console.log(light)
+    return (
+      <div className="App">
+        <center>
+
+        <h1>Light On Off</h1>
+        
+    <img src={this.state.light ? lightOn : lightOff} width="300px"/>
+    <br/>
+    <button onClick={()=>this.setState({light : !this.state.light})}>
+    {this.state.light ? "Light Off" : "Light On"}
+      </button>
+        </center>
+      </div>
+      
+    )
+  }
 }
 
 export default App;
